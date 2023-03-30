6fe30680e2c9375b46b9ea49b77cfc81c28ee2d6
Vindhyachal Tiwari <vindhyachaltiwari01@gmail.com>
2023-03-30 08:54:06 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,66 +1,55 @@
 import React, { useState } from 'react';
-import './App.css';
 
-function App() {
-  const [inputFields, setInputFields] = useState([
-    { label: 'Input 1', value: '' },
-    { label: 'Input 2', value: '' }
-  ]);
-
-  const handleAddFields = () => {
-    const newFields = [];
-    for (let i = 0; i < 5; i++) {
-      newFields.push({ label: `Input ${inputFields.length + 1 + i}`, value: '' });
-    }
-    setInputFields([...inputFields, ...newFields]);
-  };
-
-  const handleRemoveFields = (index) => {
-    const values = [...inputFields];
-    values.splice(index, 1);
-    setInputFields(values);
-  };
-
-  const handleInputChange = (index, event) => {
-    const values = [...inputFields];
-    values[index].value = event.target.value;
-    setInputFields(values);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Input Fields', inputFields);
-  };
-
+function InputBox({ index, isEnabled, handleInputChange, handleCheckboxChange }) {
   return (
-    <div className="App">
-      <div className="header">Your Heading Text</div>
-      <div className="form-container">
-        <div className="form">
-          <div className="left-column">
-            {inputFields.slice(0, 2).map((inputField, index) => (
-              <div key={index}>
-                <label>{inputField.label}</label>
-                <input type="text" value={inputField.value} onChange={(event) => handleInputChange(index, event)} />
-              </div>
-            ))}
-            <button onClick={handleAddFields}>Add Inputs</button>
-            <button onClick={() => setInputFields([{ label: 'Input 1', value: '' }, { label: 'Input 2', value: '' }])}>Reset</button>
-            <button onClick={handleSubmit}>Submit</button>
-          </div>
-          <div className="right-column">
-            {inputFields.slice(2).map((inputField, index) => (
-              <div key={index}>
-                <label>{inputField.label}</label>
-                <input type="text" value={inputField.value} onChange={(event) => handleInputChange(index + 2, event)} />
-                <button onClick={() => handleRemoveFields(index + 2)}>Remove</button>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+    <div>
+      <input type="text" disabled={!isEnabled} onChange={(event) => handleInputChange(index, event)} />
+      <label>
+        <input type="checkbox" checked={isEnabled} onChange={handleCheckboxChange} />
+        Enabled
+      </label>
     </div>
   );
 }
 
-export default App;
+function InputBoxes() {
+  const [numInputs, setNumInputs] = useState(2);
+
+  const handleInputChange = (index, event) => {
+    // handle input change here
+  };
+
+  const handleAddInputs = () => {
+    setNumInputs(numInputs + 2);
+  };
+
+  const [isEnabled, setIsEnabled] = useState(Array(numInputs).fill(false));
+
+  const handleCheckboxChange = (index) => {
+    const newEnabledState = [...isEnabled];
+    newEnabledState[index] = !newEnabledState[index];
+    setIsEnabled(newEnabledState);
+  };
+
+  const inputBoxes = [];
+  for (let i = 0; i < numInputs; i++) {
+    inputBoxes.push(
+      <InputBox
+        key={i}
+        index={i}
+        isEnabled={isEnabled[i]}
+        handleInputChange={handleInputChange}
+        handleCheckboxChange={() => handleCheckboxChange(i)}
+      />
+    );
+  }
+
+  return (
+    <div>
+      {inputBoxes}
+      <button onClick={handleAddInputs}>Add Inputs</button>
+    </div>
+  );
+}
+
+export default InputBoxes;
